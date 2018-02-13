6ae764148a944f1de79e7daab37e0e85dbcff367
James <jbarkuloo@ufl.edu>
2018-02-13 12:45:06 -0500

    Second commit

---
+++ Lab4/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import static java.lang.Character.isDigit;
+
+public class Main {
+    public static void main ( String args[] )
+    {
+    Scanner scnr = new Scanner(System.in);
+    String input = scnr.next();
+    int i;
+    char inputChar1 = input.charAt(0);
+    char inputChar2 = input.charAt(1);
+
+    System.out.println("Enter a hexadecimal number: ");
+//change
+    System.out.println("Change");
+    else
+    {
+        while ( )
+    }
+
+
+
+
+
+
+
+
+
+    }
+}
